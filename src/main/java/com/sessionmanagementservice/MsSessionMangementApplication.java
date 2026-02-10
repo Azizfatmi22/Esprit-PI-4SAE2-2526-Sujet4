df5836d649a843504941f6582082e84f0f018dc2
kthiriyoussef <kthiriyoussef09@gmail.com>
2026-02-10 22:40:23 +0100

    Initial Commit

---
+++ src/main/java/com/sessionmanagementservice/MsSessionMangementApplication.java
@@ -0,0 +1,13 @@
+package com.sessionmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSessionMangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSessionMangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sessionmanagementservice/MsSessionMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sessionmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSessionMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
