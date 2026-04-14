405df7da0fa6668ff8e9e05fa3c6f156990b64da
kthiriyoussef <kthiriyoussef09@gmail.com>
2026-04-14 23:31:22 +0100

    fix security and gateway

--- src/main/java/com/apigateway/config/GatewayConfig.java
+++ src/main/java/com/apigateway/config/GatewayConfig.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+
 
 @Configuration
 public class GatewayConfig {
@@ -16,13 +16,20 @@ public class GatewayConfig {
                 .route("ms-course-route", r -> r
                         .path("/api/courses/**")
                         .uri("lb://MS-COURSE"))
-                
+
                 // File Upload Routes
                 .route("ms-course-uploads-route", r -> r
                         .path("/api/files/**")
                         .filters(f -> f.rewritePath("/api/files/(?<segment>.*)", "/api/courses/uploads/${segment}"))
                         .uri("lb://MS-COURSE"))
-                
+                //LearningPath & schedule MS
+                .route("learning-path-service", r -> r
+                        .path("/api/learning-paths/**")
+                        .uri("lb://MSPathAndSchedule"))
+
+                .route("schedule-service", r -> r
+                        .path("/api/schedules/**")
+                        .uri("lb://MSPathAndSchedule"))
                 // Session Management Service Routes
                 .route("session-service", r -> r
                         .path("/api/sessions/**")
@@ -31,21 +38,58 @@ public class GatewayConfig {
                         .path("/api/plannings/**")
                         .uri("lb://MS-SESSIONMANGEMENT"))
                 .route("location-service", r -> r
-                        .path("/api/location/**")
+                        .path("/api/locations/**")
                         .uri("lb://MS-SESSIONMANGEMENT"))
                 // Evaluation Service Routes
                 .route("evaluation-service", r -> r
                         .path("/evaluations/**", "/exam/answers/**", "/api/quiz-answers/**")
                         .uri("lb://MS-EVALUATION"))
-                .route("reporting-service", r -> r
-                        .path("/reporting/**")
-                        .uri("lb://MS-REPORTING-CERTIFICATION"))
 
                 // ✅ Enrollment Service Routes (ADDED)
                 .route("enrollment-service", r -> r
                         .path("/msenrollment/**")
                         .uri("lb://MS-ENROLLMENT"))
 
+                // Forum Service Routes
+                .route("forum-service", r -> r
+                        .path("/api/posts/**")
+                        .uri("lb://FORUM-SERVICE"))
+                .route("forum-event-service", r -> r
+                        .path("/api/events/**")
+                        .uri("lb://FORUM-SERVICE"))
+
+                // MS-TrainerHiring Routes
+                .route("trainer-hiring-service", r -> r
+                        .path("/api/partners/**", "/api/trainers/**", "/api/jobs/**")
+                        .uri("lb://MS-TRAINERHIRING"))
+
+                .route("recommendation-service", r -> r
+                        .path("/api/recommend/**")
+                        .uri("http://localhost:5000"))
+                .route("ms-reporting-route", r -> r
+                        .path("/reporting/**")
+                        .uri("lb://MS-REPORTING-CERTIFICATION"))
+
+                // ========================================
+                // MS-LIVECLASS Routes
+                // ========================================
+                .route("livesession-route", r -> r
+                        .path("/api/livesession/**")
+                        .uri("lb://MS-LIVECLASS"))
+
+                .route("poll-route", r -> r
+                        .path("/api/poll/**")
+                        .uri("lb://MS-LIVECLASS"))
+
+                .route("chat-route", r -> r
+                        .path("/api/chat/**")
+                        .uri("lb://MS-LIVECLASS"))
+
+                .route("chatbot-route", r -> r
+                        .path("/api/chatbot/**")
+                        .uri("lb://MS-LIVECLASS"))
+
                 .build();
     }
+
 }
