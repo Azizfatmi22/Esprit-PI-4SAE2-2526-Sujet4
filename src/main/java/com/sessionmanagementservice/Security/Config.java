4ae21559e1792e08f12f2950027c0d5500a335d6
kthiriyoussef <104700801+kthiriyoussef@users.noreply.github.com>
2026-04-22 21:29:58 +0100

    Refactor securityFilterChain method in Config.java
    Removed commented-out code and updated CSRF handling.

--- src/main/java/com/sessionmanagementservice/Security/Config.java
+++ src/main/java/com/sessionmanagementservice/Security/Config.java
@@ -9,25 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class Config {
 
-    @Bean
+   @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
-                // ❌ Disable CSRF (API REST)
                 .csrf(csrf -> csrf.disable())
-
-                // ✅ Authorization rules
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
-                )
-
-                // ✅ JWT Resource Server (Keycloak)
-                /*.oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                );*/
-
+                );
         return http.build();
     }
 }
