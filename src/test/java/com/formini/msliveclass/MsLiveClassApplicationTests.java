c49f2de72c379f67cc467dc0d8745bc4e8d50b97
messaoudi-hazem <messaoudi.hazem@esprit.tn>
2026-05-06 03:43:43 +0100

    Added basic JUnit test for Jenkins

---
+++ src/test/java/com/formini/msliveclass/MsLiveClassApplicationTests.java
@@ -0,0 +1,15 @@
+package com.formini.msliveclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLiveClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Basic test to verify the Spring application context loads successfully.
+        // This will generate the target/surefire-reports/*.xml file required by Jenkins.
+    }
+
+}
